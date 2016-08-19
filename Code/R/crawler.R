c56c3c00c05c876f1759e48f33c60d50bfb0efab
Willdata <aiien61will@gamil.com>
2016-08-19 18:36:08 +0800

    update
    add task schedule

--- Code/R/crawler.R
+++ Code/R/crawler.R
@@ -3,62 +3,72 @@ require(magrittr)
 require(data.table)
 #require(dplyr)
 
-url <- "http://www.c-bike.com.tw/Station1.aspx"
 
-start <- proc.time()  # get beginning time
-
-tryCatch(
-{
-  ### crawler code
-  tbls <- readHTMLTable(url, which = c(34,37), header = FALSE) %>% 
-    rbindlist(., use.names = FALSE, fill = FALSE ) %>% 
-    data.table(date(), .) %>% 
-    set_colnames(c("dateTime", "stopName", "quantity", "vacancy"))
-  
-  ### save as RDS data file
-  readRDS("C:/Users/will.kuan/Desktop/ProjectX/Data/cityBike_Data.RDS")%>%
-    list(.,tbls) %>% 
-    rbindlist(., use.names = TRUE, fill = TRUE)%>%
-    saveRDS(.,"C:/Users/will.kuan/Desktop/ProjectX/Data/cityBike_Data.RDS")
-  
-  ### get task time
-  newLog <- data.frame(c(proc.time() - start, time = date(), msg = NA)) %>% 
-    t() %>% 
-    data.frame()
-  
-  ### save task log
-  read.delim("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", header = TRUE, sep = "\t") %>%
-    list(., newLog) %>% 
-    rbindlist(., use.names = FALSE, fill = FALSE) %>% 
-    write.table("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", sep = "\t", row.names = FALSE)
-},
-  ### warning part
-  warning = function(w){
-    warn <- paste("warning: ", conditionMessage(w))
+repeat{
     
-    newLog <- data.frame(c(proc.time() - start, time = date(), msg = warn)) %>% 
+  url <- "http://www.c-bike.com.tw/Station1.aspx"
+  start <- proc.time()  # get beginning time
+  
+  tryCatch(
+  {
+    ### try part : crawler code
+    tbls <- readHTMLTable(url, which = c(34,37), header = FALSE) %>% 
+      rbindlist(., use.names = FALSE, fill = FALSE ) %>% 
+      data.table(date(), .) %>% 
+      set_colnames(c("dateTime", "stopName", "quantity", "vacancy"))
+    
+    ### save as RDS data file
+    readRDS("C:/Users/will.kuan/Desktop/ProjectX/Data/cityBike_Data.RDS")%>%
+      list(.,tbls) %>% 
+      rbindlist(., use.names = TRUE, fill = TRUE)%>%
+      saveRDS(.,"C:/Users/will.kuan/Desktop/ProjectX/Data/cityBike_Data.RDS")
+    
+    ### get task time
+    newLog <- data.frame(c(proc.time() - start, time = date(), msg = NA)) %>% 
       t() %>% 
       data.frame()
     
+    ### save task log
     read.delim("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", header = TRUE, sep = "\t") %>%
       list(., newLog) %>% 
       rbindlist(., use.names = FALSE, fill = FALSE) %>% 
       write.table("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", sep = "\t", row.names = FALSE)
-},
-  ### error peart
-  error = function(e){
-    error <- paste("error: ", conditionMessage(e))
-    newLog <- data.frame(c(proc.time() - start, time = date(), msg = error)) %>% 
-      t() %>% 
-      data.frame()
-    
-    read.delim("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", header = TRUE, sep = "\t") %>%
-      list(., newLog) %>% 
-      rbindlist(., use.names = FALSE, fill = FALSE) %>% 
-      write.table("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", sep = "\t", row.names = FALSE)
+  },
+    ### warning part
+    warning = function(w){
+      warn <- paste("warning: ", conditionMessage(w))
+      
+      newLog <- data.frame(c(proc.time() - start, time = date(), msg = warn)) %>% 
+        t() %>% 
+        data.frame()
+      
+      read.delim("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", header = TRUE, sep = "\t") %>%
+        list(., newLog) %>% 
+        rbindlist(., use.names = FALSE, fill = FALSE) %>% 
+        write.table("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", sep = "\t", row.names = FALSE)
+  },
+    ### error peart
+    error = function(e){
+      error <- paste("error: ", conditionMessage(e))
+      newLog <- data.frame(c(proc.time() - start, time = date(), msg = error)) %>% 
+        t() %>% 
+        data.frame()
+      
+      read.delim("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", header = TRUE, sep = "\t") %>%
+        list(., newLog) %>% 
+        rbindlist(., use.names = FALSE, fill = FALSE) %>% 
+        write.table("C:/Users/will.kuan/Desktop/ProjectX/Data/log.txt", sep = "\t", row.names = FALSE)
+  }
+  )
+  
+  if(Sys.Date() == "2016-09-20"){
+    break
+  }else{
+    Sys.sleep(50)
+  }
+  
+
 }
-)
-
 ### statement below is for first time only
 # tryCatch(
 #   {
